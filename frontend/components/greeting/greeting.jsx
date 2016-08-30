32ecc244c3b3fab7b39e39fee22d964d8a49692b
Grace Patuwo <grace.patuwo@gmail.com>
2016-08-30 15:15:50 -0700

    created greeting component and container

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const sessionLinks = () => (
+  <nav className='login-signup'>
+    <Link to="/login" activeClassName='current'>Login</Link>
+    <Link to='/singup' activeClassName='current'>Sign up</Link>
+  </nav>
+)
+
+const personalGreeting = (currentUser, logout) => (
+  <h2 className='header-greeting'>Welcome back, {currentUser.username}!</h2>
+  <button className='header-button' onClick={logout}>Log Out</button>
+)
+
+function Greeting({currentUser, logout}){
+  if (currentUser) {
+    return personalGreeting(currentUser, logout);
+  } else {
+    return sessionLinks();
+  }
+}
+
+export default Greeting;
