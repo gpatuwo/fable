878d7d53c9d1765e01af75ca3eff72711768b2d3
Grace Patuwo <grace.patuwo@gmail.com>
2016-09-08 14:40:02 -0700

    fixed profile/login in nav

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import NavBarContainer from './navbar/navbar.jsx';
+import NavBarContainer from './navbar/navbar_container.js';
 
 const App = ({children}) => (
   <div>
