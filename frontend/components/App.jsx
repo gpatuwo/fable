3a666769e54e25f6c92d4fb7272b9d2d287ec2c5
Grace Patuwo <grace.patuwo@gmail.com>
2016-09-07 22:12:22 -0700

    nav bar structure complete

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,14 +4,55 @@ import GreetingContainer from './greeting/greeting_container';
 
 const App = ({children}) => (
   <div>
-    <header>
-      <Link to="/" className='header-logo'>
-        <img src="assets/fable-tag-logo.png"/>
-      </Link>
-      <GreetingContainer/>
-    </header>
-    {children}
+    <header className="header">
+      <nav className="header-nav group">
 
+        <Link to="/vegetables" className='header-logo'>
+          <img src="assets/fable_white.png"/>
+        </Link>
+
+        <ul className="header-list group">
+          <li className='header-links'>
+            <Link to="/recipes">
+              All Recipes
+            </Link>
+          </li>
+
+          <li className='header-links'>
+            <Link to="/recipes/new">
+              Submit Recipe
+            </Link>
+          </li>
+
+          <li className='header-links'>
+            <Link to="#">
+              Profile
+            </Link>
+            <ul className="header-profile">
+              <li>
+                <Link to="/login" className="header-profile-action">
+                  Login</Link>
+              </li>
+              <li>
+                <Link to="/signup" className="header-profile-action">
+                  Sign Up</Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className='header-links'>
+            <label className="header-search-wrapper">
+              <input type="text"
+                className="header-search"
+                placeholder="Search"/>
+            </label>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <GreetingContainer/>
+
+    {children}
   </div>
 );
 
--- frontend/components/nav_bar/nav_bar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Link, withRouter} from 'react-router';
-import GreetingContainer from '../greeting/greeting_container.js';
-
-const NavBar = ({currentUser, router, logout}) => {
-
-};
-
-export default withRouter(NavBar);
